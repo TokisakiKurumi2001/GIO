f5e69ef5e47acebcb20f6d878a76c99d8b831ca9
Le Minh Khoi <minhkhoile2001nsg@gmail.com>
2024-06-30 22:42:41 +0700

    Load peft for encoding model

--- encode_data.py
+++ encode_data.py
@@ -44,6 +44,7 @@ if __name__ == "__main__":
     logger.info('Loading model ...')
     tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
     model = AutoModelforCausalLM.from_pretrained(MODEL_PATH)
+    model.load_adapter(PEFT_PATH)
     device = torch.device('cuda:0')
     model = model.to(device)
     model.eval()
