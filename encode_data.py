d33c37dfa3242e4885005f1460daa1d31f331646
TokisakiKurumi2001 <minhkhoile2001nsg@gmail.com>
2024-06-30 23:53:30 +0700

    Fix some bugs

--- encode_data.py
+++ encode_data.py
@@ -9,27 +9,29 @@ DATA_PATH='data/syn.jsonl'
 SAVE_PATH='data/syn.pt'
 MODEL_PATH='meta-llama/Llama-2-7b-hf'
 PEFT_PATH='teacher_sft'
+MAX_LENGTH=1024
 
-def encode(data, tokenizer, model, batch_size: int=16) -> List:
+def encode(data, tokenizer, model, batch_size: int=4) -> List:
     device = model.device
     num_sample = len(data)
     preds = []
     for i in tqdm(range(0, num_sample, batch_size), desc='Encoding', unit='batch'):
         batch = data[i:i+batch_size]
         texts = [el['prompt'] for el in batch]
-        inputs = tokenizer(texts, return_tensors='pt', padding=True)
+        inputs = tokenizer(texts, return_tensors='pt', padding=True, max_length=MAX_LENGTH, truncation=True)
         inputs = {k: v.to(device) for k, v in inputs.items()}
 
         outputs = model(**inputs).logits
         sent_len = torch.sum(inputs['attention_mask'], dim=1)
         logger.debug(sent_len)
-        pred = torch.index_select(input=outputs, dim=1, indicies=sent_len)
+        pred = torch.index_select(input=outputs, dim=1, index=sent_len)
         logger.debug(pred.shape)
         preds.append(pred.clone().cpu())
 
         del inputs
         del outputs
         del pred
+        exit()
 
     pred = torch.stack(preds)
     return pred
@@ -46,6 +48,7 @@ if __name__ == "__main__":
 
     logger.info('Loading model ...')
     tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
+    tokenizer.pad_token_id = tokenizer.eos_token_id
     model = AutoModelForCausalLM.from_pretrained(MODEL_PATH)
     model.load_adapter(PEFT_PATH)
     device = torch.device('cuda:0')
